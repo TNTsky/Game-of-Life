5eb37b118552bea498d70f45db6198f46e94fa0e
TNTsky <TNTsky.com@gmail.com>
2022-10-23 22:26:01 +0800

    function_add:frame

--- Game of Life.py
+++ Game of Life.py
@@ -17,10 +17,11 @@ CellSize = [10, 10]
 CellNum = [80, 80]
 Cell = numpy.zeros(CellNum)
 play = False
+frame=False
 
 scale = 0
 rotate = 0
-
+last_xy=[-1,-1]
 pre = []
 dir = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 alive = []
@@ -56,22 +57,16 @@ def check():
 def update():
     for x in range(CellNum[0]):
         for y in range(CellNum[1]):
-            if Cell[x, y]:
-                pygame.draw.rect(screen_image, (255, 255, 255), [
-                                 x*10, y*10, 10, 10], 5)
-            else:
-                pygame.draw.rect(screen_image, (0, 0, 0),
-                                 [x*10, y*10, 10, 10], 5)
-                pygame.draw.rect(screen_image, (100, 100, 100), [
-                                 x*10, y*10, 10, 10], 1)
             if [x, y] in pre:
-                pygame.draw.rect(screen_image, (160, 160, 160), [
-                                 x*10, y*10, 10, 10], 5)
-    alive_image = pygame.font.Font(
-        "font/msjh.ttc", 24).render("███████████████████", True, (0, 0, 0), (0, 0, 0))
+                pygame.draw.rect(screen_image, (160, 160, 160), [x*10, y*10, 10, 10], 5)
+            elif Cell[x, y]:
+                pygame.draw.rect(screen_image, (255, 255, 255), [x*10, y*10, 10, 10], 5)
+            else:
+                pygame.draw.rect(screen_image, (0, 0, 0),[x*10, y*10, 10, 10], 5)
+                pygame.draw.rect(screen_image, (100, 100, 100), [x*10, y*10, 10, 10], 1)
+    alive_image = pygame.font.Font("font/msjh.ttc", 24).render("███████████████████", True, (0, 0, 0), (0, 0, 0))
     screen_image.blit(alive_image, (820, 50))
-    alive_image = pygame.font.Font(
-        "font/msjh.ttc", 24).render(f'存活數量:{len(alive)}', True, (200, 200, 200), (0, 0, 0))
+    alive_image = pygame.font.Font("font/msjh.ttc", 24).render(f'存活數量:{len(alive)}', True, (200, 200, 200), (0, 0, 0))
     screen_image.blit(alive_image, (820, 50))
 
 
@@ -81,8 +76,9 @@ pygame.display.set_caption('Conway\'s Game of Life')
 screen_image.fill(backgroud_color)
 
 play_img = pygame.image.load('images/play.png')
-play_button = button.Button(900, 740, play_img, 70, screen_image)
-
+play_button = button.Button(945, 740, play_img, 55, screen_image)
+frame_img = pygame.image.load('images/frame.png')
+frame_button = button.Button(855, 740, frame_img, 55, screen_image)
 
 def button_reset():
     for i in range(len(status_list)):
@@ -102,11 +98,7 @@ for x in range(CellNum[0]):
 
 alive_image = pygame.font.Font("font/msjh.ttc", 24).render("存活數量:0", True, (200, 200, 200), (0, 0, 0))
 screen_image.blit(alive_image, (820, 50))
-
-
 pygame.display.flip()
-
-
 
 
 
@@ -122,10 +114,12 @@ while True:
     Cell_y = MousePos[1]//10
     if MousePos[0] >= 0 and MousePos[0] <= 800:
         if status in ['add', 'remove']:
-            pre.clear()
-            for x in range(Cell_x-scale, Cell_x+scale+1):
-                for y in range(Cell_y-scale, Cell_y+scale+1):
-                    pre.append([x % 80, y % 80])
+            if last_xy!=[Cell_x,Cell_y]:
+                pre.clear()
+                for x in range(Cell_x-scale, Cell_x+scale+1):
+                    for y in range(Cell_y-scale, Cell_y+scale+1):
+                        pre.append([x % 80, y % 80])
+            last_xy=[Cell_x,Cell_y]
             if event.type == pygame.MOUSEWHEEL:
                 if event.y == 1:
                     if scale < 399:
@@ -134,12 +128,27 @@ while True:
                     if scale > -1:
                         scale -= 1
                 event.y = 0
+                pre.clear()
                 for x in range(Cell_x-scale, Cell_x+scale+1):
                     for y in range(Cell_y-scale, Cell_y+scale+1):
                         pre.append([x % 80, y % 80])
             if pygame.mouse.get_pressed()[1]:
                 scale = -1
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 3:
+                    if status == 'add':
+                        status = 'remove'
+                        button_reset()
+                        remove_img = pygame.image.load('images/remove_down.png')
+                        button.Button(900, 250, remove_img, 160, screen_image)
+                    else:
+                        status = 'add'
+                        button_reset()
+                        add_img = pygame.image.load('images/add_down.png')
+                        button.Button(900, 150, add_img, 160, screen_image)
+                event.button = 0
         else:
+            scale = 0
             pre.clear()
             if status == "glider":
                 for i in shape['glider']:
@@ -194,8 +203,16 @@ while True:
             play_img = pygame.image.load('images/play.png')
         else:
             play = True
-            play_img = pygame.image.load('images/play_down.png')
-        button.Button(900, 740, play_img, 70, screen_image)
+            play_img = pygame.image.load('images/stop.png')
+        button.Button(945, 740, play_img, 55, screen_image)
+
+    if frame_button.down(screen_image):
+        frame=True
+        if play:
+            play_img = pygame.image.load('images/play.png')
+        else:
+            play = True
+        button.Button(945, 740, play_img, 55, screen_image)
 
     if add_button.down(screen_image):
         status = 'add'
@@ -222,17 +239,24 @@ while True:
         button.Button(900, 450, gun_img, 160, screen_image)
 
     if generate_button.down(screen_image):
+        for i in alive:
+            Cell[i[0],i[1]]=0
         alive.clear()
         for x in range(CellNum[0]):
             a = [i for i in range(CellNum[0])]
             b = random.sample(a, random.randint(10, 30))
             for y in b:
                 alive.append([x, y])
-        check()
+                Cell[x,y]=1
 
     if clean_button.down(screen_image):
+        for i in alive:
+            Cell[i[0],i[1]]=0
         alive.clear()
-        check()
+        play=False
+        play_img = pygame.image.load('images/play.png')
+        button.Button(945, 740, play_img, 55, screen_image)
+
 
     if play:
         for i in range(CellNum[0]):
@@ -247,8 +271,11 @@ while True:
                 else:
                     if neighbor == 3:
                         alive.append([i, j])
-
-    check()
+        check()
     update()
+
+    if frame:
+        play=False
+        frame=False
 
     pygame.display.flip()
